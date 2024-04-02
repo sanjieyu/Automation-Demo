e9c10bd627f21b45150a72b2b2e22f4d52f40fe5
Yi Sun <61815007+sanjieyu@users.noreply.github.com>
2024-04-03 09:09:25 +1100

    Update execute_api_ui.py

--- CommonModule/execute_api_ui.py
+++ CommonModule/execute_api_ui.py
@@ -1,4 +1,4 @@
-# Author:Yi Sun(Tim) 2023-8-29
+# Author:Yi Sun(Tim) 2021-8-29
 
 '''Scheduled Automation Execution'''
 
@@ -7,26 +7,9 @@ import time
 import os
 from CommonModule.send_report_api_hotmail import *
 from CommonModule.send_report_UI_HTML import *
-# from CommonModule.send_report_UI_hotmail import *
 
 
 def ui_execution():
-    '''These are for BeautifulReport'''
-    # test_dir_ui = "C:\\Users\\Yi Sun\\PycharmProjects\\EGD\\UITestCase"
-    # test_report_ui = "C:\\Users\\Yi Sun\\PycharmProjects\\EGD\\Report\\UI"
-    #
-    # loader_ui = unittest.TestLoader()
-    # discover_ui = loader_ui.discover(test_dir_ui, pattern='test_*.py')
-    # now = time.strftime("%Y-%m-%d_%H_%M_%S")
-    #
-    #
-    # filename1 = '\\' + now + '_result1.html'
-    # runner_ui = BeautifulReport(discover_ui)
-    # runner_ui.report(description='EGD_UI_Automation_Test_Report', filename=filename1,
-    #               log_path='C:\\Users\\Yi Sun\\PycharmProjects\\EGD\\Report\\UI')
-    # new_report1 =send_report_ui.new_report_ui(test_report_ui)
-    # send_report_ui.send_mail_ui(new_report1)
-
     '''These are for HTMLTestReport'''
     test_dir_ui = "C:\\Users\\Yi Sun\\PycharmProjects\\EGD\\UITestCase"
     test_report_ui = "C:\\Users\\Yi Sun\\PycharmProjects\\EGD\\Report\\UI"
@@ -49,16 +32,12 @@ def api_execution():
     discover_api = loader_api.discover(test_dir_api, pattern='test_*.py')
     now = time.strftime("%Y-%m-%d_%H_%M_%S")
 
-
     filename2 = '\\' + now + '_result2.html'
     runner_api = BeautifulReport(discover_api)
     runner_api.report(description='EGD_API_Automation_Test_Report', filename=filename2,
                   log_path='C:\\Users\\Yi Sun\\PycharmProjects\\EGD\\Report\\API')
     new_report2 = send_report_api.new_report_api(test_report_api)
     send_report_api.send_mail_api(new_report2)
-    # report_path = test_report_api+filename2
-    # print('report path is:',report_path)
-    # os.remove(report_path)
 
 if __name__ == "__main__":
     '''for API automation'''
